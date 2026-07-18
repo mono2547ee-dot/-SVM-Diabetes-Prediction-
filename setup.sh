65a598e0ee97f89a6a0cf38ef726927d676cd1a4
mono2547ee-dot <mono2547ee@gmail.com>
2026-07-19 00:58:32 +0700

    .

--- setup.sh
+++ setup.sh
@@ -1,13 +1,7 @@
 mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"kairung@webmail.npru.ac.th\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+echo "[general]" > ~/.streamlit/credentials.toml
+echo "email = \"mono2547ee@gmail.com\"" >> ~/.streamlit/credentials.toml
+echo "[server]" > ~/.streamlit/config.toml
+echo "headless = true" >> ~/.streamlit/config.toml
+echo "enableCORS = false" >> ~/.streamlit/config.toml
+echo "port = \$PORT" >> ~/.streamlit/config.toml
