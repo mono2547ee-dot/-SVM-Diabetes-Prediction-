c263ef56f9352789c369355572e509e3ecae0758
mono2547ee-dot <mono2547ee@gmail.com>
2026-07-19 01:29:05 +0700

    .

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 mkdir -p ~/.streamlit/
-echo "[general]" > ~/.streamlit/credentials.toml
-echo "email = \"mono2547ee@gmail.com\"" >> ~/.streamlit/credentials.toml
-echo "[server]" > ~/.streamlit/config.toml
-echo "headless = true" >> ~/.streamlit/config.toml
-echo "enableCORS = false" >> ~/.streamlit/config.toml
-echo "port = \$PORT" >> ~/.streamlit/config.toml
+
+echo "\
+[general]\n\
+email = \"mono2547ee@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
